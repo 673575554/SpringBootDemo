05da082e69a7b30cbd97f7cbc08aab6b58379101
cloud <673575554@qq.com>
2021-02-15 16:15:08 +0800

    springbootdemo1

--- SpringBootDemo/SpringBootDemo1_HelloWorld/src/main/java/com/demo/Application.java
+++ SpringBootDemo/SpringBootDemo1_HelloWorld/src/main/java/com/demo/Application.java
@@ -1,4 +1,15 @@
 package com.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主程序类，所有启动的入口
+ * @SpringBootApplication：这是一个Spring应用
+ * */
+@SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
 }
