2ba1ba58defa774c7ba2a49e44bf9e25adf41cc6
cloud <673575554@qq.com>
2021-02-19 11:11:52 +0800

    springbootdemo4

--- SpringBootDemo/SpringBootDemo4_Web01/src/main/java/com/demo/controller/RequestController.java
+++ SpringBootDemo/SpringBootDemo4_Web01/src/main/java/com/demo/controller/RequestController.java
@@ -2,11 +2,14 @@ package com.demo.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
