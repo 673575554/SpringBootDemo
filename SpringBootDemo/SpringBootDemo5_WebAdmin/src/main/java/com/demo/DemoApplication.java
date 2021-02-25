4dab6828bfb72b08a571380022907252e098085e
loogn <you@example.com>
2021-02-25 17:51:29 +0800

    springboot5_mybatis修复

--- SpringBootDemo/SpringBootDemo5_WebAdmin/src/main/java/com/demo/DemoApplication.java
+++ SpringBootDemo/SpringBootDemo5_WebAdmin/src/main/java/com/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@MapperScan(basePackages = {"com.demo"})
 @ServletComponentScan("com.demo")
 @SpringBootApplication
 public class DemoApplication {
--- SpringBootDemo/SpringBootDemo5_WebAdmin/src/main/java/com/demo/mapper/AccountMapper.java
+++ SpringBootDemo/SpringBootDemo5_WebAdmin/src/main/java/com/demo/mapper/AccountMapper.java
@@ -2,7 +2,10 @@ package com.demo.mapper;
 
 
 import com.demo.bean.Account;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
+@Component
 @Mapper
 public interface AccountMapper {
 
